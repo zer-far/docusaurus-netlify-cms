11819146242fe552898592d9d9263a87a03e6acc
Abdullah <faridabdullah1@protonmail.com>
2021-02-28 21:01:53 +0000

    Updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,6 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -65,10 +64,6 @@ module.exports = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
